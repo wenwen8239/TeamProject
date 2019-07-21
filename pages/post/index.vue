1398cc2b4498bd941c0c881d0ceeb32534aa5569
Your Name <you@example.com>
2019-07-21 21:39:18 +0800

    旅游攻略静态页面完成，城市列表数据渲染完成，文章列表数据渲染完成

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,15 +1,47 @@
 <template>
-    <div>
-        旅游攻略首页
-    </div>
+  <div class="index">
+    <el-container>
+      <el-aside width="260px">
+        <!-- 左侧导航栏 -->
+        <PostNav/>
+      </el-aside>
+      <el-main id='you' width="700px">
+        <!-- 写游记 -->
+        <Mains/>
+        <!-- 内容 -->
+        <Mainx/>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script>
+import PostNav from '@/components/post/postnav'
+import Mains from '@/components/post/mains'
+import Mainx from '@/components/post/mainx'
 export default {
-
-}
+  data() {
+    return {
+    };
+  },
+  components: {
+    PostNav,
+    Mains,
+    Mainx
+  },
+  methods: {}
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.index {
+  width: 1000px;
+  margin: 0 auto;
+  padding: 20px 0;
+  
+}
+.el-main{
+    padding:0;
+    padding-left: 40px;
+  }
 </style>
