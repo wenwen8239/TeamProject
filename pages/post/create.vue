75c9b2a9e49378fc49d6c24e0d1df456eb3bd63b
Your Name <you@example.com>
2019-07-22 21:38:20 +0800

    写游记页面功能完成

--- pages/post/create.vue
+++ pages/post/create.vue
@@ -10,14 +10,13 @@
           </el-form-item>
           <!-- 富文本插件 -->
           <el-form-item>
-            <VueEditor :config="config" ref="vueEditor" v-model="form.content"/>
+            <VueEditor :config="config" ref="vueEditor"/>
           </el-form-item>
           <el-form-item label="选择城市" label-width="80px">
             <el-autocomplete
               v-model="form.city"
               :fetch-suggestions="queryCitySearch"
               placeholder="请输入内容"
-              @select="handleCitySelect"
             ></el-autocomplete>
           </el-form-item>
         </el-form>
@@ -25,17 +24,20 @@
           <el-button type="primary" size="medium" style="margin-right: 20px;font-size: 12px;" @click="handleSubmit">发布</el-button>
           <div class="submit-side" style="font-size: 14px;">
             <span>或者</span>
-            <nuxt-link to="#" style="color: orange;" @click="saveDraft">保存到草稿</nuxt-link>
+            <nuxt-link to="#" style="color: orange;" @click.native="saveDraft">保存到草稿</nuxt-link>
           </div>
         </el-row>
       </el-col>
       <el-col class="aside" :span="5">
         <!-- 草稿箱 -->
-        <div class="draft-box">
-          <h4 class="draft-title">草稿箱（1）</h4>
+        <div class="draft-box"
+        v-for="(item,index) in $store.state.post.createForm"
+        :key="index"
+        >
+          <h4 class="draft-title">草稿箱（{{index+1}}）</h4>
           <div class="draft-list">
             <div class="draft-item">
-              <div @click="editDraft"><i class="iconfont el-icon-edit"></i></div>
+              <div @click="editDraft(item,index)">{{item.title}}<i class="iconfont el-icon-edit"></i></div>
               <p>2019-07-20</p>
             </div>
           </div>
@@ -63,6 +65,14 @@ export default {
         content: '',
         city: ''
       },
+      // 草稿
+      draftForm:[
+        {
+        title: '',
+        content: '',
+        city: ''
+      }
+        ],
       // 富文本配置
       config: {
         modules: {
@@ -115,26 +125,81 @@ export default {
       }
     }
   },
+  mounted () {
+  },
   methods: {
+       // 封装实现下拉菜单
+    querySearch(value) {
+      return new Promise((resolve,reject) => {
+        // 判断当输入的数据为空的时候
+        if (!value) {
+          return resolve([]); // 不加载下拉菜单
+        }
+        // 请求城市信息
+        this.$axios({
+          url: '/airs/city',
+          params: {
+            name: value
+          }
+        }).then(res => {
+          // 获取请求到的数据
+          const {data} = res.data;
+          // 给数组中的对象添加value，下拉列表必须要有value选项，value是用来展示下拉列表数据的绑定值
+          const newArr = data.map(v => {
+            v.value = v.name.replace('市','');
+              return v;
+          })
+          resolve(newArr)
+        })
+      })
+    },
     // 实现远程搜索城市列表数据
-    queryCitySearch(value,callback) {
-
+    // await后面要接收promise对象，返回的值是resolve的参数
+    async queryCitySearch(value,callback) {
+      const res = await this.querySearch(value);
+      if (res.length > 0) {
+        this.form.departCity = res[0].value;
+        this.form.departCode = res[0].sort;
+      }
+      callback(res)
     },
-    // 点击获取城市名称
-    handleCitySelect() {
 
-    },
     // 保存到草稿箱
     saveDraft() {
+      this.form.content = this.$refs.vueEditor.editor.root.innerHTML
+      const arr = {...this.form}
+      this.draftForm = arr
+      this.$store.commit('post/setInfoData',this.draftForm)
 
+      
     },
     // 编辑草稿箱信息
-    editDraft() {
-
+    editDraft(item,index) {
+      this.form.title = item.title
+      this.form.city = item.city
+      this.$refs.vueEditor.editor.root.innerHTML = item.content
+      this.$store.commit('post/delInfoData',index)
     },
     // 发布新攻略
     handleSubmit() {
-
+      const {
+        user: { userInfo }
+      } = this.$store.state;
+      this.$axios({
+        url: "/posts",
+        method: "POST",
+        data: this.form,
+        headers: {
+          Authorization: `Bearer ${userInfo.token || "NO TOKEN"}`
+        }
+      }).then((res) => {
+        if(res.status == 200){
+          this.$message({
+          message:res.data.message,
+          type: 'success'
+        });
+        }
+      })
     }
   }
 }
---
+++ store/post.js
@@ -0,0 +1,20 @@
+export const state = () => {
+  return {
+    createForm:[]
+  }
+}
+
+export const mutations = {
+  // 草稿数据
+  setInfoData(state, data) {
+    state.createForm.unshift(data)
+    if(state.createForm.length>5){
+      state.createForm.length = 5
+    }
+  },
+  // 删除草稿数据
+  delInfoData(state, data) {
+    state.createForm.splice(data,1)
+  },
+ 
+}
