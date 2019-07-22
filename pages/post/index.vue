65d338fc2420e0aa8c3b83a5d2f2fdbec4d660e1
cozy <1950300860@qq.com>
2019-07-22 14:57:09 +0800

    实现攻略详情页静态布局

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,15 +1,43 @@
 <template>
-    <div>
-        旅游攻略首页
-    </div>
+  <section>
+    <el-row type="flex" justify="space-between">
+      <!-- 攻略详情 -->
+      <el-col :span="16" style="margin-bottom:20px;">
+        <DetailsContent/>
+      </el-col>
+
+      <!-- 相关攻略 -->
+      <DetailsRelated/>
+    </el-row>
+
+    <!-- 攻略评论 -->
+    <el-row type="flex">
+      <el-col :span="16">
+        <DetailsComment/>
+      </el-col>
+    </el-row>
+  </section>
 </template>
 
 <script>
+import DetailsRelated from "@/components/post/detailsRelated.vue";
+import DetailsContent from "@/components/post/detailsContent.vue";
+import DetailsComment from "@/components/post/detailsComment.vue";
 export default {
-
+  // 注册组件
+  components: {
+    DetailsRelated,
+    DetailsContent,
+    DetailsComment
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  section{
+    width: 1000px;
+    margin: 0 auto;
+    padding-top: 20px;
+  }
 </style>
+
